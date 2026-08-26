12431d8a26e7f228e45ca00389d2b5e4e75d1786
Sergi Roviralta <sergirovisu74@gmail.com>
2026-08-26 12:08:07 +0200

    show git branch in fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,3 @@
-#source /usr/share/cachyos-fish-config/cachyos-config.fish
-
 # ==========================================================
 # Catppuccin Mocha Theme for Fish Syntax Highlighting
 # ==========================================================
@@ -19,15 +17,27 @@ set -g fish_color_escape eba0ac
 set -g fish_color_autosuggestion 6c7086
 set -g fish_color_cancel f38ba8
 
+# ==========================================================
+# Prompt Personalizado con soporte Git
+# ==========================================================
 function fish_prompt
     set -l last_status $status
 
     set -l color_user 89b4fa
     set -l color_path fab387
     set -l color_arrow cba6f7
-
-    echo -n (set_color $color_user)(whoami)(set_color normal)" "(set_color $color_path)(pwd)(set_color normal)" "
     
+    # Color verde para la rama de Git
+    set -g __fish_git_prompt_color_branch a6e3a1
+
+    # 1. Imprime usuario y ruta
+    echo -n (set_color $color_user)(whoami)(set_color normal)" "(set_color $color_path)(pwd)(set_color normal)
+
+    # 2. Imprime la rama de Git (si estás en un repositorio)
+    echo -n (fish_git_prompt)
+
+    # 3. Espacio final y flecha según si el comando anterior falló
+    echo -n " "
     if test $last_status -eq 0
        echo -n (set_color $color_arrow)"> "(set_color normal)
     else
@@ -38,7 +48,9 @@ end
 function fish_greeting
 end
 
+# ==========================================================
+# Ejecución al abrir terminal
+# ==========================================================
 fastfetch --disable-linewrap true --logo arch_small --logo-padding-right 3 --structure Title:OS:Kernel:Uptime:CPU:GPU:Memory
-
-echo 
-echo 
+echo
+echo
